524f4283047907025cf37236a5f62ed2c33acc01
Barbora Lambeinova <lambeinovab@seznam.cz>
2024-12-19 14:37:29 +0100

    -

--- scripts/database.js
+++ scripts/database.js
@@ -1,1 +1,44 @@
-let m
+let dishes = [
+    {
+        "name": "Spaghetti mit Fleischbällchen",
+        "description": "Traditionelle italienische Spaghetti, serviert mit saftigen Fleischbällchen in einer würzigen Tomatensauce.",
+        "price": 12.90
+    },
+    {
+        "name": "Linguine al Limone et Pecorino",
+        "description": "Frische Linguine in einer cremigen Sauce aus Zitronen und Pecorino-Käse, abgerundet mit frischen Kräutern.",
+        "price": 14.50
+    },
+    {
+        "name": "Pasta Salat Caprese mit gegrilltem Hähnchen",
+        "description": "Ein frischer Pastasalat mit Tomaten, Mozzarella und Basilikum, serviert mit gegrilltem Hähnchen und Balsamico-Dressing.",
+        "price": 11.90
+    },
+    {
+        "name": "Pizza Margherita",
+        "description": "Klassische Pizza mit Tomatensauce, Mozzarella, frischem Basilikum und einem Hauch von Olivenöl.",
+        "price": 8.50
+    },
+    {
+        "name": "Pizza Quattro Stagioni",
+        "description": "Pizza mit vier Belägen: Schinken, Pilze, Artischocken und schwarze Oliven, auf einer Tomatensauce-Basis.",
+        "price": 10.90
+    },
+    {
+        "name": "Pizza Funghi",
+        "description": "Pizza mit einer reichhaltigen Tomatensauce, Mozzarella und frischen Champignons.",
+        "price": 9.50
+    },
+    {
+        "name": "Pinsa Romana",
+        "description": "Ein traditioneller römischer Fladen aus einer besonderen Teigmischung, belegt mit Tomaten, Mozzarella und frischen Kräutern.",
+        "price": 11.00
+    },
+    {
+        "name": "Torta Caprese nach Art des Hauses",
+        "description": "Ein köstlicher, saftiger Schokoladenkuchen aus Mandeln, serviert mit einer Kugel Vanilleeis und Schokoladensauce.",
+        "price": 6.90
+    }
+];
+
+let basket = [];
