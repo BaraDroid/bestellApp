d3de9128abef4af10659dcc8610479be3a00a251
Barbora Lambeinova <lambeinovab@seznam.cz>
2024-12-21 15:47:36 +0100

    name, rating and restaurant picture should be static, not dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,6 @@
 
 function init() {
-    getRestaurantName();
-    getRating();
-    getRestaurantPicture();
     renderDishes();
-}
-
-function getRestaurantName() {
-    document.getElementById("restaurantName").innerHTML = restaurants[0].name;
-}
-
-function getRating() {
-    document.getElementById("ratingBar").innerHTML = `Bewertung : ${restaurants[0].review}%`;
-}
-
-function getRestaurantPicture() {
-    document.getElementById("restaurantPicture").innerHTML = `<img class="custom_picture" src="${restaurants[0].picture}">`;
 }
 
 function renderDishes() {
